31397d304fc1368918704587b5e6fe95949a6268
Rajan9032 <67915280+Rajan9032@users.noreply.github.com>
2024-01-29 12:00:23 +0530

    Update wisecow.sh

--- wisecow.sh
+++ wisecow.sh
@@ -25,13 +25,13 @@ EOF
 }
 
 #prerequisites() {
-#	command -v cowsay >/dev/null 2>&1 &&
-#	command -v fortune >/dev/null 2>&1 || 
-#		{ 
-#			echo "Install prerequisites."
-#			exit 1
-#		}
-#}
+	command -v cowsay >/dev/null 2>&1 &&
+	command -v fortune >/dev/null 2>&1 || 
+		{ 
+			echo "Install prerequisites."
+			exit 1
+		}
+}
 
 main() {
 	prerequisites
